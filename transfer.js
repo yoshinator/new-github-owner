8f66b85170d404fc6a65ec16b487766732c0e020
Yoan Ante <yoanante@gmail.com>
2019-06-28 12:46:48 -0400

    clean up and export for npm

--- transfer.js
+++ transfer.js
@@ -1,54 +1,57 @@
-// const waitForUserInput = require('wait-for-user-input');
-
-
 const axios = require('axios');
 const inquirer = require('inquirer')
 
-function getValues(){
-    inquirer
-        .prompt([
-            {
-                message: "Please provide your github username",
-                name: "username"
-            },
-            {
-                message: "What is the organization you would like to transfer",
-                name: "organization"
-            },
-            {
-                message: "What is your github authorization key \n you can get one at: \n  https://github.com/settings/tokens/new \n please select repo and organization when creating token",
-                name: "token"
-            }
-
-        ])
-        .then(answers => {
-            callGithub(answers.username, answers.organization, answers.token)
-        });
-}
-
-function callGithub(username, organization, token){
-    axios.get(`https://api.github.com/users/${username}/repos?page=$page&per_page=5`)
-        .then(response => {
-                console.log("=========BEGIN RESPONSE===============\n",response,"\n===========END RESPONSE================")
-                updateGithub(response.data, username, organization, token)
+    function getValues(){
+        inquirer
+            .prompt([
+                {
+                    message: "Please provide your github username",
+                    name: "username"
+                },
+                {
+                    message: "What is the organization you would like to transfer",
+                    name: "organization"
+                },
+                {
+                    message: "What is your github authorization key \n you can get one at: \n  https://github.com/settings/tokens/new \n please select repo and organization when creating token",
+                    name: "token"
+                }
+            ])
+            .then(answers => {
+                callGithub(answers.username, answers.organization, answers.token)
             })
-    
-}
+            .catch(e => {
+                console.log("ERROR at getValues line 8")
+            })
+            
+    }
 
-function updateGithub(repos, username, organization, token){
-    for (repo of repos) {
-        console.log(repo.name)
-
-        axios({
-            method: 'post',
-            url: `https://api.github.com/repos/${username}/${repo.name}/transfer`,
-            headers: { 'Accept': 'application/vnd.github.nightshade-preview+json', 'Authorization': `token ${token}`},
-            data: { "new_owner": organization}
+    function callGithub(username, organization, token){
+        axios.get(`https://api.github.com/users/${username}/repos?page=$page&per_page=1`)
+        .then(response => {
+                updateGithub(response.data, username, organization, token)
         })
-            .then(function (response) {
-                console.log("Moved repo: ",response.data.name, " \nto  ", organization)
-            });
-    }    
-}
+        .catch(e =>{
+            console.log("ERROR at callGitHub function see line 32",e)
+        }) 
+    }
 
-getValues()
+    function updateGithub(repos, username, organization, token){
+        for (repo of repos) {
+
+            axios({
+                method: 'post',
+                url: `https://api.github.com/repos/${username}/${repo.name}/transfer`,
+                headers: { 'Accept': 'application/vnd.github.nightshade-preview+json', 'Authorization': `token ${token}`},
+                data: { "new_owner": organization}
+            })
+                .then(function (response) {
+                    console.log("Moved repo: ",response.data.name, " \nto  ", organization)
+                })
+                .catch(e => {
+                    console.log("ERROR at updateGithub line 46", e)
+                });
+        }    
+    }
+
+module.exports = getValues
